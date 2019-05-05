e85b79022890c2571d769d89e32065b19b9fb579
Aniket Pandey <aniketp@iitk.ac.in>
2019-05-05 09:46:36 +0530

    Add initial driver program

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/aniketp/go-compiler/lexer"
+	"github.com/aniketp/go-compiler/parser"
+)
+
 func check(err error) {
 	if err != nil {
 		panic(err)
@@ -7,5 +16,22 @@ func check(err error) {
 }
 
 func Parse(input string) {
-	l := lexer
+	l := lexer.NewLexer([]byte(input))
+	p := parser.NewParser()
+
+	_, err := p.Parse(l)
+	check(err)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("Provide a valid file name")
+	}
+
+	path := os.Args[1]
+	absPath, _ := filepath.Abs(path)
+	input, err := ioutil.ReadFile(absPath)
+	check(err)
+
+	Parse(string(input))
 }

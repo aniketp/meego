3d04b7ca3f8bd83ea0fce9c5d291e457ea83c274
Aniket Pandey <aniketp@iitk.ac.in>
2019-05-05 08:41:51 +0530

    Add scanner test and Lexer build commands

---
+++ Makefile
@@ -0,0 +1,18 @@
+GOCMD=go
+GOCLEAN=$(GOCMD) clean
+GOTEST=$(GOCMD) test
+GENERATOR=grammer.bnf
+GENERATE=../../../../bin/gocc
+
+
+all: test run
+
+test:
+	$(GENERATE) $(GENERATOR)
+	$(GOTEST) -v 
+
+clean:
+	$(GOCLEAN)
+	rm -rf util token lexer
+run:
+	$(GENERATE) $(GENERATOR) # create lexer and parser
---
+++ scanner_test.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aniketp/go-compiler/lexer"
+	"github.com/aniketp/go-compiler/token"
+)
+
+func TestToken(t *testing.T) {
+	type Test struct {
+		expectedType    token.Type
+		expectedLiteral string
+	}
+	const input = `
+			/* comment should not be scanned */
+			let five = "test";
+			let ten = 10;
+			let add = fn(x, y) {
+				x + y;
+			};
+
+			let result = add(five, ten);  
+			5 < 10 > 5;
+
+			if (5 < 10) {
+				return true;
+			} else {
+				return false;
+			}
+
+			10 == 10;
+			func add(x int
+			`
+
+	tests := []Test{
+		{token.TokMap.Type("let"), "let"},
+		{token.TokMap.Type("ident"), "five"},
+		{token.TokMap.Type("assign"), "="},
+		{token.TokMap.Type("string_literal"), "\"test\""},
+		{token.TokMap.Type("semicolon"), ";"},
+		{token.TokMap.Type("let"), "let"},
+		{token.TokMap.Type("ident"), "ten"},
+		{token.TokMap.Type("assign"), "="},
+		{token.TokMap.Type("int"), "10"},
+		{token.TokMap.Type("semicolon"), ";"},
+		{token.TokMap.Type("let"), "let"},
+		{token.TokMap.Type("ident"), "add"},
+		{token.TokMap.Type("assign"), "="},
+		{token.TokMap.Type("ident"), "fn"},
+		{token.TokMap.Type("lparen"), "("},
+		{token.TokMap.Type("ident"), "x"},
+		{token.TokMap.Type("comma"), ","},
+		{token.TokMap.Type("ident"), "y"},
+		{token.TokMap.Type("rparen"), ")"},
+		{token.TokMap.Type("lbrace"), "{"},
+		{token.TokMap.Type("ident"), "x"},
+		{token.TokMap.Type("plus"), "+"},
+		{token.TokMap.Type("ident"), "y"},
+		{token.TokMap.Type("semicolon"), ";"},
+		{token.TokMap.Type("rbrace"), "}"},
+		{token.TokMap.Type("semicolon"), ";"},
+		{token.TokMap.Type("let"), "let"},
+		{token.TokMap.Type("ident"), "result"},
+		{token.TokMap.Type("assign"), "="},
+		{token.TokMap.Type("ident"), "add"},
+		{token.TokMap.Type("lparen"), "("},
+		{token.TokMap.Type("ident"), "five"},
+		{token.TokMap.Type("comma"), ","},
+		{token.TokMap.Type("ident"), "ten"},
+		{token.TokMap.Type("rparen"), ")"},
+		{token.TokMap.Type("semicolon"), ";"},
+		{token.TokMap.Type("int"), "5"},
+		{token.TokMap.Type("lt"), "<"},
+		{token.TokMap.Type("int"), "10"},
+		{token.TokMap.Type("gt"), ">"},
+		{token.TokMap.Type("int"), "5"},
+		{token.TokMap.Type("semicolon"), ";"},
+		{token.TokMap.Type("if"), "if"},
+		{token.TokMap.Type("lparen"), "("},
+		{token.TokMap.Type("int"), "5"},
+		{token.TokMap.Type("lt"), "<"},
+		{token.TokMap.Type("int"), "10"},
+		{token.TokMap.Type("rparen"), ")"},
+		{token.TokMap.Type("lbrace"), "{"},
+		{token.TokMap.Type("return"), "return"},
+		{token.TokMap.Type("true"), "true"},
+		{token.TokMap.Type("semicolon"), ";"},
+		{token.TokMap.Type("rbrace"), "}"},
+		{token.TokMap.Type("else"), "else"},
+		{token.TokMap.Type("lbrace"), "{"},
+		{token.TokMap.Type("return"), "return"},
+		{token.TokMap.Type("false"), "false"},
+		{token.TokMap.Type("semicolon"), ";"},
+		{token.TokMap.Type("rbrace"), "}"},
+		{token.TokMap.Type("int"), "10"},
+		{token.TokMap.Type("eq"), "=="},
+		{token.TokMap.Type("int"), "10"},
+		{token.TokMap.Type("semicolon"), ";"},
+		{token.TokMap.Type("func"), "func"},
+		{token.TokMap.Type("ident"), "add"},
+		{token.TokMap.Type("lparen"), "("},
+		{token.TokMap.Type("ident"), "x"},
+		{token.TokMap.Type("ident"), "int"},
+		{token.TokMap.Type("$"), ""}, // end token
+	}
+
+	l := lexer.NewLexer([]byte(input))
+	for i, tt := range tests {
+		tok := l.Scan()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected='%s', got='%s' at line %d, column %d",
+				i, token.TokMap.Id(tt.expectedType), token.TokMap.Id(tok.Type), tok.Pos.Line, tok.Pos.Column)
+		}
+
+		if string(tok.Lit) != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected='%q', got='%q' at line %d, column %d",
+				i, tt.expectedLiteral, string(tok.Lit), tok.Pos.Line, tok.Pos.Column)
+		}
+	}
+}

cba4f119d3fc95b000dcca607f759c93ff2f42f2
Aniket Pandey <aniketp@iitk.ac.in>
2019-05-05 10:37:06 +0530

    Define AST Type structures and interfaces

---
+++ ast/types.go
@@ -0,0 +1,77 @@
+package ast
+
+import "github.com/aniketp/go-compiler/token"
+
+type Attrib interface{}
+
+type Program struct {
+	Statements []Statement
+}
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	StatementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Statement structures
+type AssignStatement struct {
+	Token *token.Token `json:"-"`
+	Left  Identifier   `json:"left"`
+	Right Expression   `json:"right"`
+}
+
+type FunctionStatement struct {
+	token      *token.Token    `json:"-"`
+	Name       string          `json:"name"`
+	Parameters []FormalArg     `json:"params"`
+	Body       *BlockStatement `json:"body"`
+	Return     string          `json:"return"`
+}
+
+type FormalArg struct {
+	Arg  string `json:"arg"`
+	Type string `json:"type"`
+}
+
+type ForStatement struct {
+	Token          *token.Token    `json:"-"`
+	Condition      Expression      `json:"condition"`
+	BlockStatement *BlockStatement `json:"block"`
+}
+
+type ReturnStatement struct {
+	Token       *token.Token `json:"-"`
+	ReturnValue Expression   `json:"return"`
+}
+
+type BlockStatement struct {
+	Token      *token.Token `json:"-"`
+	Statements []Statement  `json:"statements"`
+}
+
+type IfStatement struct {
+	Token       *token.Token    `json:"-"`
+	Condition   Expression      `json:"condition"`
+	Block       *BlockStatement `json:"block"`
+	Alternative *BlockStatement `json:"alternative"`
+}
+
+type ExpressionStatement struct {
+	Token      *token.Token `json:"-"`
+	Expression Expression   `json:"statement"`
+}
+
+type InitStatement struct {
+	Token    *token.Token `json:"-"`
+	Expr     Expression   `json:"expression"`
+	Location string       `json:"location"`
+}

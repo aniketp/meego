cfdc82601c9fb3bb7f4f669f613aa0ab085dece6
Aniket Pandey <aniketp@iitk.ac.in>
2019-05-06 05:53:57 +0530

    Finish up statement evaluation procedure

--- checker/checker.go
+++ checker/checker.go
@@ -1,6 +1,11 @@
 package checker
 
-import "github.com/aniketp/meego/ast"
+import (
+	"errors"
+	"reflect"
+
+	"github.com/aniketp/meego/ast"
+)
 
 //  Driver Type-Checker function
 func Checker(program *ast.Program) error {
@@ -44,5 +49,115 @@ func checker(node ast.Node) (string, error) {
 		return evalFunctionCall(node)
 	}
 
+	return "", nil
+}
+
+// Target program
+func evalProgram(p *ast.Program) (string, error) {
+	for _, function := range p.Functions {
+		_, err := checker(function)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	for _, statement := range p.Statements {
+		_, err := checker(statement)
+		if err != nil {
+			return "", err
+		}
+	}
+	return "", nil
+}
+
+// Statements
+func evalBlockStatement(node *ast.BlockStatement) (string, error) {
+	for _, statement := range node.Statements {
+		result, err := checker(statement)
+		if err != nil {
+			return "", err
+		}
+
+		if reflect.TypeOf(statement) == reflect.TypeOf(&ast.ReturnStatement{}) {
+			return result, nil
+		}
+	}
+
+	return NOTHING_TYPE, nil
+}
+
+func evalReturnStatement(node *ast.ReturnStatement) (string, error) {
+	res, err := checker(node.ReturnValue)
+	return res, err
+}
+
+func evalIfStatement(node *ast.IfStatement) (string, error) {
+	cond, _ := checker(node.Condition)
+	if cond != BOOL_TYPE {
+		return "", errors.New("Condition not of Boolean type")
+	}
+
+	checker(node.Block)
+	checker(node.Alternative)
+	return "", nil
+}
+
+func evalExpressionStatement(node *ast.ExpressionStatement) (string, error) {
+	_, err := checker(node.Expression)
+	if err != nil {
+		return "", err
+	}
+
+	return "", nil
+}
+
+func evalInitStatement(node *ast.InitStatement) (string, error) {
+	if env.IdentExist(node.Location) {
+		return "", errors.New("Identifier already exists")
+	}
+
+	right, err := checker(node.Expr)
+	if err != nil {
+		return "", nil
+	}
+
+	env.Set(node.Location, right) // Set identifier type
+	return "", nil
+}
+
+// TODO: Fix this
+func evalAssignStatement(node *ast.AssignStatement) (string, error) {
+	right, err := checker(node.Right)
+	if err != nil {
+		return "", nil
+	}
+
+	if kind, ok := env.Get(node.Left.Value); ok {
+		if kind != right {
+			return "", errors.New("invalid type assignment")
+		}
+	} else {
+		return "", errors.New("ident not exist")
+	}
+	return "", nil
+}
+
+func evalFunctionStatement(node *ast.FunctionStatement) (string, error) {
+	var params []string
+	for _, param := range node.Parameters {
+		env.Set(param.Arg, param.Type) // set params into scope
+		params = append(params, param.Type)
+	}
+
+	res, err := checker(node.Body)
+	if err != nil {
+		return "", err
+	}
+	// check if correct return type
+	if res != node.Return {
+		return "", errors.New("incorrect return type")
+	}
+
+	SetFunctionSignature(node.Name, Signature{node.Return, params})
 	return "", nil
 }

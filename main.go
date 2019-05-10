28939b2ae3c1ed10de5e18954e24261dc2619dd5
Aniket Pandey <aniketp@iitk.ac.in>
2019-05-10 10:04:17 +0530

    Modify main program to include the code generator

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/aniketp/meego/ast"
 	"github.com/aniketp/meego/checker"
+	"github.com/aniketp/meego/codegen"
 	"github.com/aniketp/meego/lexer"
 	"github.com/aniketp/meego/parser"
 )
@@ -32,6 +36,36 @@ func TypeCheck(program *ast.Program) {
 	check(err)
 }
 
+func Compile(code bytes.Buffer) string {
+	f, err := os.Create("./template/main.cpp")
+	check(err)
+	defer f.Close()
+
+	f.Write(code.Bytes())
+
+	var out bytes.Buffer
+	cmd1 := exec.Command("g++", "-o", "apple", "./template/main.cpp",
+		"./template/Builtins.cpp")
+	cmd1.Stderr = &out
+	err = cmd1.Run()
+
+	// Check if output was a valid one
+	if len(out.String()) != 0 {
+		panic(fmt.Sprintf("error: %s", out.String()))
+	}
+
+	// Now, execute the resulting 'apple' binary
+	cmd := exec.Command("./apple")
+	var outb bytes.Buffer
+	cmd.Stdout = &outb
+	err = cmd.Run()
+	check(err)
+
+	// This is the generated output of our transpiled program
+	return outb.String()
+
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		panic("Provide a valid file name")
@@ -44,4 +78,8 @@ func main() {
 
 	program := Parse(string(input))
 	TypeCheck(program)
+	// Generate vanilla C++
+	code := codegen.GenWrapper(program)
+	result := Compile(code)
+	fmt.Println(result)
 }
